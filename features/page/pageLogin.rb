a7454fd67248d88c8be0dfcd0012ab1fdbd3429d
Helder1234 <heldermendes1985@gmail.com>
2017-04-25 14:45:55 -0300

    Segundo commit

--- features/page/pageLogin.rb
+++ features/page/pageLogin.rb
@@ -4,9 +4,9 @@ class LoginPage < SitePrism::Page
 	element :password_field, :xpath, "//*[@id='login']/div/form/input[5]"
 	element :bnt_entrar, "button[type='submit']"
 
-def acesso(user,pass)
-	email_field.set(user)
-	password_field.set(pass)
-	bnt_entrar.click
-end
+	def acesso(user,pass)
+		email_field.set(user)
+		password_field.set(pass)
+		bnt_entrar.click
+	end
 end
